c3638193ada1a68e534b10c1f221c8c361bb1335
Anton <bruhanovantonn@gmail.com>
2021-02-07 14:23:35 +0300

    Initial commit

---
+++ src/test/java/MyFirstTest.java
@@ -0,0 +1,19 @@
+import org.junit.jupiter.api.Test;
+
+public class MyFirstTest {
+
+    @Test
+    void firstTest(){
+        System.out.println("Test 1");
+    }
+
+    @Test
+    void secondTest(){
+        System.out.println("Test 2");
+    }
+
+    @Test
+    void firdTest(){
+        System.out.println("Test 3");
+    }
+}
